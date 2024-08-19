c2800de0b9cd47829dbb9413ae56a142739b52ce
rjohn11118 <145144057+rjohn11118@users.noreply.github.com>
2024-08-19 08:20:07 -0500

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+let temp = 13.0
+var newtemp = temp * 9
+newtemp = newtemp / 5
+newtemp += 32
+
+print("Temperature in Fahrenheit: \(newtemp) °F")
+print("Temperature in Celcius: \(temp) °C")
